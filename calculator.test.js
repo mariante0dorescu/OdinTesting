f601a43ed40672d6238be0ec3c278cf8af5d51c4
mariante0dorescu <broinkus@gmail.com>
2023-09-04 14:25:13 +0300

    calculator function and test file finished

---
+++ calculator.test.js
@@ -0,0 +1,17 @@
+const calculator = require('./calculator');
+
+test('Check if 1 plus 1 equal 2', () => {
+  expect(calculator.add(1,1)).toBe(2);
+})
+
+test('Check if 1 minus 1 equal 0', () => {
+  expect(calculator.substract(1,1)).toBe(0);
+})
+
+test('Check if 3 multiply by 3 equal 9', () => {
+  expect(calculator.multiply(3,3)).toBe(9);
+})
+
+test('Check if 6 divided by 2 equal 3', () => {
+  expect(calculator.divide(6,2)).toBe(3);
+})
